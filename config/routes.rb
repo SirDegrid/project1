ad0dfa462261147bf8832762a055197efe83c50c
Maggie Cameron <maggie.rose.cameron@gmail.com>
2023-04-27 11:56:59 +1000

    more css. last commit before trying associations troubleshoot

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-  get 'wines/index'
-  get 'wines/new'
-  get 'wines/edit'
-  get 'wines/show'
-  get 'dishes/index'
-  get 'dishes/new'
-  get 'dishes/edit'
-  get 'dishes/show'
-  get 'cuts/index'
-  get 'cuts/new'
-  get 'cuts/edit'
-  get 'cuts/show'
   root :to => 'cuts#index'
   resources :cuts
   resources :dishes
